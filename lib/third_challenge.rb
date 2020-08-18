6d06dd6de5a10c0bf3ca61a527ef9068d457a16b
Jason Keeling <s2jason@yahoo.com>
2020-08-18 22:32:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -28,7 +28,7 @@ def third_challenge
       :heroine => {
         name: "Juliet",
         age: "15",
-        stage: "alive"
+        status: "alive"
         },
       :heroine_friends => []
    }
