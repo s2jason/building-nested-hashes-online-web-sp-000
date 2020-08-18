cb268b28ef4e1e150822fac594be53c02f516a58
Jason Keeling <s2jason@yahoo.com>
2020-08-18 22:43:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,9 +4,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{name: "Benvolio", age: ""
+      :hero_friends => [{name: "Benvolio", age: "17", status: "worried"
         
-      ]
+      }]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
